bc8c27e026f983eecea9b1b121e318353e5db79e
Shawn Cheng <melo89dec@hotmail.com>
2025-10-16 14:13:59 +1100

    Add Spectral Normalization (Stabilize Training)

--- month_2_gan_from_scratch/models/discriminator.py
+++ month_2_gan_from_scratch/models/discriminator.py
@@ -1,27 +1,31 @@
 # models/discriminator.py
 import torch.nn as nn
+from torch.nn.utils.spectral_norm import spectral_norm
+
 
 class Discriminator(nn.Module):
     """
     Simple DCGAN-style Discriminator.
     Input: RGB image of size (batch_size, img_channels, 64, 64)
     Output: probability of real image (batch_size, 1)
+
+    Use Spectral Normalization to Stabilize Training (Prevents discriminator from becoming too strong.)
     """
     def __init__(self, img_channels=3, hidden_dim=64):
         super(Discriminator, self).__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(img_channels, hidden_dim, 4, 2, 1),
+            spectral_norm(nn.Conv2d(img_channels, hidden_dim, 4, 2, 1)),
             nn.LeakyReLU(0.2),
 
-            nn.Conv2d(hidden_dim, hidden_dim * 2, 4, 2, 1),
+            spectral_norm(nn.Conv2d(hidden_dim, hidden_dim * 2, 4, 2, 1)),
             nn.BatchNorm2d(hidden_dim * 2),
             nn.LeakyReLU(0.2),
 
-            nn.Conv2d(hidden_dim * 2, hidden_dim * 4, 4, 2, 1),
+            spectral_norm(nn.Conv2d(hidden_dim * 2, hidden_dim * 4, 4, 2, 1)),
             nn.BatchNorm2d(hidden_dim * 4),
             nn.LeakyReLU(0.2),
 
-            nn.Conv2d(hidden_dim * 4, hidden_dim * 8, 4, 2, 1),
+            spectral_norm(nn.Conv2d(hidden_dim * 4, hidden_dim * 8, 4, 2, 1)),
             nn.BatchNorm2d(hidden_dim * 8),
             nn.LeakyReLU(0.2),
 
