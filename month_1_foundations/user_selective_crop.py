c06652f41a6852dde24efaf7adb5e4616e538e86
Shawn Cheng <melo89dec@hotmail.com>
2025-11-04 14:28:07 +1100

    use relative path

--- month_1_foundations/user_selective_crop.py
+++ month_1_foundations/user_selective_crop.py
@@ -1,7 +1,7 @@
 import cv2
  
 # Load the image
-img = cv2.imread("/home/shawncheng/Downloads/Image.png")
+img = cv2.imread("images/*.png")  # Provide the correct path to your image file
  
 # Let user select ROI (drag a box)
 roi = cv2.selectROI("Select ROI", img, False)
@@ -10,7 +10,7 @@ roi = cv2.selectROI("Select ROI", img, False)
 cropped_img = img[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
  
 # Save and display cropped image
-cv2.imwrite("/home/shawncheng/Downloads/Cropped_Image.png", cropped_img)  # Save the cropped image
+cv2.imwrite("images/output/Cropped_Image.png", cropped_img)  # Save the cropped image
 cv2.imshow("Cropped Image", cropped_img)  # Display the cropped image
 cv2.waitKey(0)  # Wait for a key press to close the displayed image
 cv2.destroyAllWindows()
